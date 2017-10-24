23cd44a81301b15a01d6437127c6a1fbd3c7c950
myriemboularouk <myriem.boularouk@lepoles.com>
2017-10-24 14:05:27 +0200

    Initial commit

---
+++ membre.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class Membre
+{
+	private $prenom;
+
+	public function __construct($arg)
+	{
+		$this->setPrenom($arg);
+	}
+
+	public function setPrenom($arg)
+	{
+		$this->prenom = $arg;
+	}
+
+	public function getPrenom()
+	{
+		return $this->prenom;
+	} 
+}
+
+$membre = new Membre('Marc');
+var_dump($membre);
---
+++ panier.class.php
@@ -0,0 +1,19 @@
+<?php
+ 	
+//panier class.php
+
+/**
+*
+*/
+class panier{
+
+   public $nbproduits; //une propriété
+
+   public function ajouterArticle() // une méthode
+   	{
+   		return 
+   	}
+
+ 												
+}
+ ?>	
